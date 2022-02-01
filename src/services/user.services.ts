f7b5a8890f041d1bd742e3403c261327ceba3af5
RiadSaidu <reidvai1998@gmail.com>
2022-02-01 14:33:47 +0600

    added: find user by id

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,9 +1,21 @@
+import { ObjectId } from "mongoose"
 import { UserDocument } from "../interfaces/model.interface"
 import { User } from "../model/user.model"
 
 export const getUserByEmail = async (email: string) => {
   try {
     const user = await User.findOne({ email })
+    if(user) return user
+    else return false
+  } catch (error) {
+    console.log(error.message)
+    return false
+  }
+}
+
+export const getUserById = async (uid: ObjectId) => {
+  try {
+    const user = await User.findById(uid)
     if(user) return user
     else return false
   } catch (error) {
